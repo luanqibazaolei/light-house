37dc4422ebf23a1ae3521c6e2ff7dcb3d0100d8e
shenzl <shenzl@wotransfer.com>
2021-12-23 16:11:54 +0800

    nacos 服务间通讯成功

--- light-mng/src/main/java/com/light/mng/controller/BandInfoController.java
+++ light-mng/src/main/java/com/light/mng/controller/BandInfoController.java
@@ -1,7 +1,11 @@
 package com.light.mng.controller;
 
 import com.light.mng.api.BandInfoClient;
+import com.light.mng.entity.BandInfo;
+import com.light.mng.utils.domain.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +13,11 @@ public class BandInfoController {
 
     @Autowired
     BandInfoClient bandInfoClient;
+
+    @PostMapping("/bandInfo")
+    public Result insert(@RequestBody BandInfo bandInfo){
+        return bandInfoClient.insert(bandInfo);
+    }
 
 
 
